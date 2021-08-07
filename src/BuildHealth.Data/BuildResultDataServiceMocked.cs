c8764a86e3009c537718f221393edaebfbdce9d5
tp2124 <tp2124@gmail.com>
2021-08-07 13:37:10 -0500

    Adding an Extensions to the BuildResultModel. Slick use of extensions for easier Razor html formatting. Adding in TimeSpan formatting and visualization.

--- src/BuildHealth.Data/BuildResultDataServiceMocked.cs
+++ src/BuildHealth.Data/BuildResultDataServiceMocked.cs
@@ -13,9 +13,9 @@ namespace BuildHealth.Data
         {
             _mockedDataSet = new List<BuildResultModel>();
             var now = DateTime.Now;
-            _mockedDataSet.Add(new BuildResultModel() { Id = 1, ChangelistCutoff = 11, StartTime = now - TimeSpan.FromMinutes(4.23), FinishTime = now - TimeSpan.FromMinutes(0.3) });
+            _mockedDataSet.Add(new BuildResultModel() { Id = 1, ChangelistCutoff = 11, StartTime = now - TimeSpan.FromMinutes(4.23), FinishTime = now - TimeSpan.FromMinutes(0.3), Result = true });
             _mockedDataSet.Add(new BuildResultModel() { Id = 2, ChangelistCutoff = 12, StartTime = now - TimeSpan.FromMinutes(3.73), FinishTime = now - TimeSpan.FromMinutes(0.2) });
-            _mockedDataSet.Add(new BuildResultModel() { Id = 3, ChangelistCutoff = 24, StartTime = now - TimeSpan.FromMinutes(2.15), FinishTime = now - TimeSpan.FromMinutes(0.069) });
+            _mockedDataSet.Add(new BuildResultModel() { Id = 3, ChangelistCutoff = 24, StartTime = now - TimeSpan.FromMinutes(2.15), FinishTime = now - TimeSpan.FromMinutes(0.069), Result = true });
         }
 
         public async Task<List<BuildResultModel>> GetBuilds()
---
+++ src/BuildHealth/BuildResultModelExtensions.cs
@@ -0,0 +1,23 @@
+﻿using BuildHealth.Data.Models;
+using System;
+
+namespace BuildHealth
+{
+    public static class BuildResultModelExtensions
+    {
+        public static string ResultColor(this BuildResultModel resultModel)
+        {
+            return resultModel.Result ? "green" : "red";
+        }
+
+        public static TimeSpan Duration(this BuildResultModel resultModel)
+        {
+            return resultModel.FinishTime - resultModel.StartTime;
+        }
+
+        public static TimeSpan TimeSinceCompletion(this BuildResultModel resultModel)
+        {
+            return DateTime.Now - resultModel.FinishTime;
+        }
+    }
+}
